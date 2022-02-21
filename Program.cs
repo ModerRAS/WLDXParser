e9a5a10c09236e909fa3e8933865123834f835be
ModerRAS <adqewsx@gmail.com>
2022-02-21 20:47:12 +0800

    删掉无用代码

--- Program.cs
+++ Program.cs
@@ -20,37 +20,6 @@ namespace WLDXParser
                 var children = item.Children;
                 if (children.ToList().Count > 2)
                 {
-                    //foreach (var child in children)
-                    //{
-                    //    Console.WriteLine($"child is {NomoralRegex(child.TextContent)}");
-                    //    if (string.IsNullOrEmpty(NomoralRegex(child.TextContent)))
-                    //    {
-                    //        var tmpStr = new List<string>();
-                    //        foreach(var item2 in child.Children)
-                    //        {
-                    //            string text = item2.TextContent;
-                    //            Console.WriteLine($"text is {NomoralRegex(text)}");
-                    //            if (!string.IsNullOrEmpty(NomoralRegex(text)))
-                    //            {
-                    //                text = text.Trim();
-                    //                tmpStr.Add(text);
-                    //            } else
-                    //            {
-                    //                foreach (var item3 in  item2.Children)
-                    //                {
-                    //                    string text2 = item3.TextContent;
-                    //                    if (!string.IsNullOrEmpty(NomoralRegex(text2)))
-                    //                    {
-                    //                        text2 = text2.Trim();
-                    //                        tmpStr.Add(text2);
-                    //                        Console.WriteLine($"text2 is {NomoralRegex(text2)}");
-                    //                    }
-                    //                }
-                    //            }
-                    //        }
-                    //        Console.WriteLine(String.Join("", tmpStr));
-                    //    }
-                    //}
                     Console.WriteLine(children[0].GetAttribute("text"));
                     if (children[0].Children.ToList().Count > 0)
                     {

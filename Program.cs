fd29eb687990a97753bf1aa4da5eda70f4db0cf5
ModerRAS <adqewsx@gmail.com>
2022-02-21 22:20:04 +0800

    能用了

--- Program.cs
+++ Program.cs
@@ -13,63 +13,90 @@ namespace WLDXParser
         }
         static void Main(string[] args)
         {
-            var doc = new XmlDocument();
-            doc.Load(@"C:\WorkSpace\XML\继电保护高级技师\单选题第一组.xml");
-            //var document = parser.ParseDocument(File.ReadAllText(@"C:\WorkSpace\XML\继电保护高级技师\单选题第一组.xml"));
-            var root = doc.DocumentElement;
-            var nodes = root.SelectNodes("//node[@class=\"android.widget.ListView\"]");
-            foreach (XmlNode PerQuestion in nodes)
+            string[] BasicPaths = { @"C:\WorkSpace\XML\继电保护高级技师\", @"C:\WorkSpace\XML\继电保护基础知识" };
+            foreach (var BasicPath in BasicPaths)
             {
-                //check is Per Question
-                if (PerQuestion.HasChildNodes && (
-                      PerQuestion.ChildNodes[0].HasChildNodes && 
-                      !string.IsNullOrWhiteSpace(PerQuestion.ChildNodes[0].ChildNodes[0].Attributes["text"].Value) ||
-                        !string.IsNullOrWhiteSpace(PerQuestion.ChildNodes[0].Attributes["text"].Value)
-                      )
-                    )
+                using (StreamWriter sw = new StreamWriter(Path.Combine(BasicPath, "输出.txt")))
                 {
-                    foreach (XmlNode Content in PerQuestion.ChildNodes)
+                    var QuestionNumber = 0;
+                    foreach (var file in Directory.GetFiles(BasicPath))
                     {
-                        var TitleAnswer = Content.Attributes["text"].Value.Trim();
-                        if (Content.HasChildNodes)
+                        if (file.EndsWith("txt"))
                         {
-                            foreach (XmlNode e in Content.ChildNodes)
-                            {
-                                TitleAnswer += e.Attributes["text"].Value.Trim();
-                                TitleAnswer += " ";
-                                if (e.HasChildNodes)
-                                {
-                                    foreach (XmlNode f in e.ChildNodes)
-                                    {
-                                        TitleAnswer += f.Attributes["text"].Value.Trim();
-                                        TitleAnswer += " ";
-                                    }
-                                }
-                            }
+                            continue;
                         }
-                        Console.WriteLine(TitleAnswer); 
-                    }
-                    
+                        var doc = new XmlDocument();
+                        doc.Load(Path.Combine(BasicPath, file));
+                        //var document = parser.ParseDocument(File.ReadAllText(@"C:\WorkSpace\XML\继电保护高级技师\单选题第一组.xml"));
+                        var root = doc.DocumentElement;
+                        var nodes = root.SelectNodes("//node[@class=\"android.widget.ListView\"]");
+                        foreach (XmlNode PerQuestion in nodes)
+                        {
+                            //check is Per Question
+                            if (PerQuestion.HasChildNodes && (
+                                  PerQuestion.ChildNodes[0].HasChildNodes &&
+                                  !string.IsNullOrWhiteSpace(PerQuestion.ChildNodes[0].ChildNodes[0].Attributes["text"].Value) ||
+                                    !string.IsNullOrWhiteSpace(PerQuestion.ChildNodes[0].Attributes["text"].Value)
+                                  )
+                                )
+                            {
+                                QuestionNumber++;
+                                sw.Write(QuestionNumber.ToString());
+                                sw.Write(". ");
+                                foreach (XmlNode Content in PerQuestion.ChildNodes)
+                                {
+                                    var TitleAnswer = Content.Attributes["text"].Value.Trim();
+                                    if (Content.HasChildNodes)
+                                    {
+                                        foreach (XmlNode e in Content.ChildNodes)
+                                        {
+                                            TitleAnswer += e.Attributes["text"].Value.Trim();
+                                            TitleAnswer += " ";
+                                            if (e.HasChildNodes)
+                                            {
+                                                foreach (XmlNode f in e.ChildNodes)
+                                                {
+                                                    TitleAnswer += f.Attributes["text"].Value.Trim();
+                                                    TitleAnswer += " ";
+                                                }
+                                            }
+                                        }
+                                    }
+                                    sw.WriteLine(TitleAnswer);
+                                    Console.WriteLine(TitleAnswer);
+                                }
 
-                    var TrueAnswer = "";
-                    foreach (XmlNode e in PerQuestion.ParentNode.ChildNodes)
-                    {
-                        TrueAnswer += e.Attributes["text"].Value;
-                    }
-                    
 
-                    //Console.WriteLine(title);
-                    Console.WriteLine();
-                    Console.WriteLine(TrueAnswer);
-                    Console.WriteLine();
-                    Console.WriteLine();
+                                var TrueAnswer = "";
+                                foreach (XmlNode e in PerQuestion.ParentNode.ChildNodes)
+                                {
+                                    TrueAnswer += e.Attributes["text"].Value;
+                                }
+
+                                sw.WriteLine();
+                                sw.WriteLine(TrueAnswer);
+                                sw.WriteLine();
+                                sw.WriteLine();
+                                //Console.WriteLine(title);
+                                Console.WriteLine();
+                                Console.WriteLine(TrueAnswer);
+                                Console.WriteLine();
+                                Console.WriteLine();
+                            }
+                            //Console.WriteLine(node.ChildNodes.Attributes["text"].Value);
+                            //Console.WriteLine();
+                            //var titles = node.SelectNodes("/node[@index=\"0\"]");
+                            //if (titles.Count > 0)
+                            //Console.WriteLine(titles[0].InnerText);
+                        }
+
+                    }
                 }
-                //Console.WriteLine(node.ChildNodes.Attributes["text"].Value);
-                //Console.WriteLine();
-                //var titles = node.SelectNodes("/node[@index=\"0\"]");
-                //if (titles.Count > 0)
-                //Console.WriteLine(titles[0].InnerText);
+                
             }
+            
+
+            
         }
     }
 }
